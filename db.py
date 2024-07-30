d9adcfc3e40d0538cef19ee4638b0ce1882b55c1
Hallabassuny7 <hallabassuny@gmail.com>
2024-07-31 01:57:06 +0300

    kkk

--- db.py
+++ db.py
@@ -36,3 +36,33 @@ def edit_user(connection, username, Nuser, Nemail,pfp_url):
     cursor.execute(query, (Nuser, Nemail, pfp_url, username))
     connection.commit()
     print("User data updated successfully")
+
+def seed_admin_user(connection):
+    admin_username = 'admin'
+    admin_password = 'Admin@2003'
+
+    admin_user = get_user(connection, admin_username)
+    if not admin_user:
+        add_user(connection, admin_username, admin_password)
+        print("Admin user seeded successfully.")
+
+#---------------------------------------------------------------------------------------
+def init_game_table(connection):
+    cursor = connection.cursor()
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS games (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                description TEXT,
+                price REAL NOT NULL,
+                image_url TEXT
+            )
+    ''')
+    connection.commit()
+
+def add_game(connection, title, description, price, image_url=None):
+    cursor = connection.cursor()
+    query = ''' INSERT INTO games (title, description, price, image_url) VALUES (?, ?, ?, ?)'''
+    cursor.execute(query, (title, description, price, image_url))
+    connection.commit()
+
